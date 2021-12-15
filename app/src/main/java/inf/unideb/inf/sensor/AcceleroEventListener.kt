c0c15c468937ff7866511bf0ee05cf95d8955f02
SimonMMST <turel9605@gmail.com>
2021-12-15 14:40:16 +0100

    SpiritLevel app code reformat

--- app/src/main/java/inf/unideb/inf/sensor/AcceleroEventListener.kt
+++ app/src/main/java/inf/unideb/inf/sensor/AcceleroEventListener.kt
@@ -5,9 +5,9 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.widget.ProgressBar
 
-class AcceleroEventListener(val horizontal: ProgressBar,val vertical: ProgressBar): SensorEventListener {
-//    var bias_hor = -4
-//    var bias_ver =  4
+class AcceleroEventListener(val horizontal: ProgressBar, val vertical: ProgressBar) :
+    SensorEventListener {
+    //var bias_hor = -4  //var bias_ver =  4
     override fun onSensorChanged(p0: SensorEvent?) {
         horizontal.progress = ((p0!!.values[0] + 9.7) * 10).toInt()
         vertical.progress = ((p0.values[1] + 9.7) * 10).toInt()
